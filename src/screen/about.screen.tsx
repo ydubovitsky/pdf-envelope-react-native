42a44d390e76c432c4fa59936c425f001fce0da2
y-dubovitsky <pXmJ5M9dvP>
2023-09-11 23:19:39 +0300

    AboutScreen and DocsScreen added with no content

---
+++ src/screen/about.screen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const AboutScreen: React.FC = (): JSX.Element => {
+  return (
+    <View>
+      <Text>About</Text>
+    </View>
+  );
+};
+
+export default AboutScreen;
---
+++ src/screen/docs.screen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const DocsScreen: React.FC = (): JSX.Element => {
+  return (
+    <View>
+      <Text>DocsScreen</Text>
+    </View>
+  );
+};
+
+export default DocsScreen;
